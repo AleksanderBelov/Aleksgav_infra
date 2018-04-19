acc06b248d62dd04dff0181b312962ae70d98c53
Aleks G <alex.phyp@gmail.com>
2018-04-20 02:04:09 +0300

    Added initial playbooks and dynamic inventory

--- ansible/inventory.sh
+++ ansible/inventory.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-cat ./inventory.json
+APP_EXTERNAL_IP="$(gcloud compute instances describe reddit-app-0  --format="value(networkInterfaces[0].accessConfigs[0].natIP)" --zone=europe-west1-b)"
+DB_EXTERNAL_IP="$(gcloud compute instances describe reddit-db-0  --format="value(networkInterfaces[0].accessConfigs[0].natIP)" --zone=europe-west1-b)"
+DB_INTERNAL_IP="$(gcloud compute instances describe reddit-db-0  --format="value(networkInterfaces[0].networkIP)" --zone=europe-west1-b)"
+
+cat << EndOfJSON
+{
+  "app": {
+    "hosts": ["${APP_EXTERNAL_IP}"],
+    "vars": {
+      "db_host": "${DB_INTERNAL_IP}"
+    }
+  },
+  "db": {
+    "hosts": ["${DB_EXTERNAL_IP}"]
+  }
+}
+EndOfJSON
